91a19667e9b1c76dfd6225b1ad8540b5b6720078
gifflet <guisousa09@hotmail.com>
2025-03-27 22:17:33 -0300

    feat: initial implementation of AI-powered code review integration with suggested changes

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gifflet/git-review/cmd"
+	"github.com/gifflet/git-review/internal/config"
 )
 
 // AppVersion is defined during compilation
 var AppVersion = "dev"
 
 func main() {
+	// Load configuration
+	cfg, err := config.LoadConfig()
+	if err != nil {
+		fmt.Printf("Error loading config: %v\n", err)
+		os.Exit(1)
+	}
+
 	// Set the application version
 	cmd.AppVersion = AppVersion
+
+	// Set config in cmd package
+	cmd.SetConfig(cfg)
 
 	// Execute the root command
 	cmd.Execute()
